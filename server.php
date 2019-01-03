54ac23e26fdff35b0637623297fc41abc4a205d8
Administrator <1808583880@qq.com>
2019-01-03 08:21:52 +0800

    这是第二次修改

--- server.php
+++ server.php
@@ -1,11 +1,11 @@
 <?php
 header("Access-Control-Allow-Origin:http://10.30.29.59:63341");
 header("Access-Control-Allow-Credentials:true");
-
 header("Context-Type:text/json");
 
 
 $userName = array('userName'=>'123');
 echo json_encode($userName);
 //RodLink
+//2019年1月3日 08:20:56
 
